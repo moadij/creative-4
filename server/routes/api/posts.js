695cdb6743793a99b5b199de8e138fa1cb341f66
moadij <moalstigsson@gmail.com>
2020-04-14 16:08:58 -0600

    added back-end

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,28 +3,38 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-//Get Post
+// Get Posts
 router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}));
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Post
-
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
 
 // Delete Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send({});
+});
 
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://moadij:m&m082518@cluster0-lpztk.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true
+    }
+  );
 
-
-async function loadPostsCollection {
-    const client = await mongodb.MongoClient.connect
-    ('mongodb+srv://moadij:m&m082518@cluster0-lpztk.mongodb.net/test?retryWrites=true&w=majority', 
-   
-    {useNewUrlParser: true}
-    
-    );
-
-    return client.db('vue_express').collection('posts');
+  return client.db('vue_express').collection('posts');
 }
 
 module.exports = router;
